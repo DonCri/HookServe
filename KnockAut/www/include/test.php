e31685cd9d2d81689cac67bd149005533fecbada
DonCri <36209368+DonCri@users.noreply.github.com>
2020-04-30 00:07:22 +0200

    Add files via upload

--- KnockAut/www/include/test.php
+++ KnockAut/www/include/test.php
@@ -1,12 +1,12 @@
 <?php
 
 $CategoryList = IPS_GetCategoryList();
+sort($CategoryList);
 
 $Var = "";
 
 foreach($CategoryList as $Category) {
     $CatName = IPS_GetName($Category);
-    sort($Category);
     $Var .= $Category . " -> " . $CatName . "<br>";
 }
 
