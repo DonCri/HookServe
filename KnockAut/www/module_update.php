419f83b61e8336b17f0102471b7b97d2c0396214
DonCri <36209368+DonCri@users.noreply.github.com>
2020-04-29 20:36:03 +0200

    Add files via upload

--- KnockAut/www/module_update.php
+++ KnockAut/www/module_update.php
@@ -2,6 +2,6 @@
 
 MC_UpdateModule(IPS_GetInstanceIDByName("Modules", 0), "HookServe");
 
-echo("Module-Update erfolgreich! \n\n");
-echo("<a href='index.html'>Zurück</a>");
+echo("<p>Module-Update erfolgreich!</p>");
+echo("<p><a href='index.html'>Zurück</a></p>");
 
