f2698bc3e0d40952a8c46100da915adc779ab33f
DonCri <36209368+DonCri@users.noreply.github.com>
2020-04-30 21:38:23 +0200

    Add files via upload

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -21,7 +21,8 @@
         foreach($CategoryList as $Category) { 
         
             $CatName = IPS_GetName($Category);?>
-            <a href="#dialog"><div id="content"><div id="contentIMG"><img src="img/Home.png"> </div>
+            <a href="#dialog"><div id="content">
+                <div id="contentIMG"><img src="img/Home.png"> </div>
             <?php $Var = $CatName; 
             echo $Var; ?>
             </a></div>
