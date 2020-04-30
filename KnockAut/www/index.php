a85d870b80cbd7237efd83beb40f766cfb0b6139
DonCri <36209368+DonCri@users.noreply.github.com>
2020-04-30 16:52:50 +0200

    Add files via upload

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -32,7 +32,7 @@
 <div>
 
 <a href="#dialog">Dialog Öffnen</a><a href="#">Dialog Schließen</a>
-<dialog open>
+<dialog open id="meinDialog">
 Hallo, ich bin ein Dialog, der über CSS geöffnet wurde.
 </dialog>
 
