ee1f001bb19f2916cd5679a5b3a6fc94d8ca4469
DonCri <36209368+DonCri@users.noreply.github.com>
2020-04-30 21:50:36 +0200

    Add files via upload

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -22,7 +22,7 @@
         
             $CatName = IPS_GetName($Category);?>
             <a href="#dialog"><div id="content">
-                <img src="img/Home.png">
+                <img id="contentIMG" src="img/Home.png">
             <span><?php $Var = $CatName; 
             echo $Var; ?></span>
             </a></div>
