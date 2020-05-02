8d0882a8e47631cf9c1bfe81963fc1a35a1b8b90
Christian <c.fanciullo@me.com>
2020-05-02 22:10:14 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -43,7 +43,7 @@
             $CatName = IPS_GetName($Category);?>
             <a href="#dialog"><div id="content">
                     <!-- Modal Trigger -->
-            <button id="modalStyle" data-target="modal1" class="btn modal-trigger">Modal</button>
+            <button id="modalStyle" data-target="modal1" class="btn modal-trigger"> <?php echo $Var ?></button>
 
             <!-- Modal Structure -->
             <div id="modal1" class="modal">
