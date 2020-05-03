bed5bad15aa98c1d09374d5a746245860ac5399a
Christian <c.fanciullo@me.com>
2020-05-03 16:47:12 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -35,9 +35,9 @@
 <div id="nav">
 
     <ul class="tabs tabs-fixed-width tab-demo z-depth-1">
-        <li class="tab"><a href="#test1">My House</a></li>
-        <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-        <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
+        <li class="tab"><a class="active" href="#test1">My House</a></li>
+        <li class="tab"><a href="#test2">Test 2</a></li>
+        <li class="tab"><a href="#test3">Disabled Tab</a></li>
         <li class="tab"><a href="#test4">Test 4</a></li>
         <li class="tab"><a href="#test0">Test 5</a></li>
     </ul>
