ea0cd79d07cc1c1978a1b99f9a8db21e32973d5e
Christian <c.fanciullo@me.com>
2020-05-03 12:19:50 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -60,7 +60,7 @@
                         <div class="modal-content">
                             <h4> <?php echo $Var; ?> </h4>
                             <div> <?php
-                                    $InstanceList = IPS_GetChildrenIDs($CategoryList[1], 33603);
+                                    $InstanceList = IPS_GetChildrenIDs($CategoryList[1]);
                                     $Inst = ""; 
 
                                     foreach($InstanceList as $Instance) { 
