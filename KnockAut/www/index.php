bd370ae70bd6b2d63ec87741a8dd55f54d897a21
Christian <c.fanciullo@me.com>
2020-05-03 16:19:13 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -16,11 +16,8 @@
 
 <script> $(document).ready(function(){
    $('.modal').modal();
+   $('.tabs').tabs();
  });</script>
-
- <script>$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})</script>
 
 </head>
 
@@ -34,6 +31,21 @@
     <div id="titel">My House</div>
     <div id="titelUpdate"> <p><a href="module_update.php"><img id="ModuleUpdate" src="img/update_v2.png"></a></p> </div>
 </div>
+
+<div class="row">
+    <div class="col s12">
+      <ul class="tabs">
+        <li class="tab col s3"><a href="#test1">Test 1</a></li>
+        <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
+        <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
+        <li class="tab col s3"><a href="#test4">Test 4</a></li>
+      </ul>
+    </div>
+    <div id="test1" class="col s12">Test 1</div>
+    <div id="test2" class="col s12">Test 2</div>
+    <div id="test3" class="col s12">Test 3</div>
+    <div id="test4" class="col s12">Test 4</div>
+  </div>
 
 <div id="nav">
     <?php
