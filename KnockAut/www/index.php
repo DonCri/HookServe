7bab5160e5d1b096b43d64a6f19a79b85b9cdcd6
Christian <c.fanciullo@me.com>
2020-05-03 16:34:29 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -34,36 +34,36 @@
 
 <div id="nav">
 
-<div id="content">
-<div class="row">
-    <div class="col s12">
-      <ul class="tabs">
-        <li class="tab col s3"><a href="#test1">My House</a></li>
-        <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
-        <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
-        <li class="tab col s3"><a href="#test4">Test 4</a></li>
-      </ul>
-    </div>
-        <div id="test1" class="col s12"> <div id="content"> <?php
-                                                $CategoryList = IPS_GetChildrenIDs(33603);
-                                                $Var = "";
+    <div id="content">
+        <div class="row">
+            <div class="col s12">
+                <ul class="tabs">
+                    <li class="tab col s3"><a href="#test1">My House</a></li>
+                    <li class="tab col s3 disabled"><a class="active" href="#test2">Test 2</a></li>
+                    <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
+                    <li class="tab col s3"><a href="#test4">Test 4</a></li>
+                </ul>
+            </div>
+                <div id="test1" class="col s12"> <div id="content"> <?php
+                                                        $CategoryList = IPS_GetChildrenIDs(33603);
+                                                        $Var = "";
 
-                                                foreach($CategoryList as $Category) { 
-                                                
-                                                    $CatName = IPS_GetName($Category);
-                                                    $Var = $CatName;?>
+                                                        foreach($CategoryList as $Category) { 
+                                                        
+                                                            $CatName = IPS_GetName($Category);
+                                                            $Var = $CatName;?>
 
-                                                    <div id="content">
-                                                                    <!-- Modal Trigger -->
-                                                                    <button id="modalStyle" data-target="modal1" class="btn modal-trigger"> <?php echo $Var; ?> </button>
-                                                    </div>
-                                            <?php } ?> </div>
+                                                            <div id="content">
+                                                                            <!-- Modal Trigger -->
+                                                                            <button id="modalStyle" data-target="modal1" class="btn modal-trigger"> <?php echo $Var; ?> </button>
+                                                            </div>
+                                                    <?php } ?> </div>
+                </div>
+            <div id="test2" class="col s12">Test 2</div>
+            <div id="test3" class="col s12">Test 3</div>
+            <div id="test4" class="col s12">Test 4</div>
         </div>
-    <div id="test2" class="col s12">Test 2</div>
-    <div id="test3" class="col s12">Test 3</div>
-    <div id="test4" class="col s12">Test 4</div>
-  </div>
-</div>
+    </div>
 </div>
 
 
