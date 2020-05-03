ff4a7cecb5abbeccd0901ba8c56b05d1e5150f0e
Christian <c.fanciullo@me.com>
2020-05-03 16:26:36 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -32,7 +32,7 @@
     <div id="titelUpdate"> <p><a href="module_update.php"><img id="ModuleUpdate" src="img/update_v2.png"></a></p> </div>
 </div>
 
-<div id="nav">
+<div id="content">
 
 <div class="row">
     <div class="col s12">
