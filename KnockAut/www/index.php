3ba80ee3a2f7a54fffee8c14a0af8290beaac0ef
Christian <c.fanciullo@me.com>
2020-05-03 16:41:36 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -34,17 +34,15 @@
 
 <div id="nav">
 
-    <div id="content">
-        <div class="row">
-            <div class="col s12">
-                <ul class="tabs">
-                    <li class="tab col s3"><a class="active" href="#test1">My House</a></li>
-                    <li class="tab col s3"><a class="active" href="#test2">Tab 2asdasddas</a></li>
-                    <li class="tab col s3"><a href="#test3">Tab 3</a></li>
-                    <li class="tab col s3"><a href="#test4">Tab 4</a></li>
-                </ul>
-            </div>
-                <div id="test1" class="col s12"> <div id="content"> <?php
+    <ul class="tabs tabs-fixed-width tab-demo z-depth-1">
+        <li class="tab"><a href="#test1">My House</a></li>
+        <li class="tab"><a class="active" href="#test2">Test 2</a></li>
+        <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
+        <li class="tab"><a href="#test4">Test 4</a></li>
+        <li class="tab"><a href="#test0">Test 5</a></li>
+    </ul>
+    <div id="test1" class="col s12">
+                                                    <div id="content"> <?php
                                                         $CategoryList = IPS_GetChildrenIDs(33603);
                                                         $Var = "";
 
@@ -58,12 +56,11 @@
                                                                             <button id="modalStyle" data-target="modal1" class="btn modal-trigger"> <?php echo $Var; ?> </button>
                                                             </div>
                                                     <?php } ?> </div>
-                </div>
-            <div id="test2" class="col s12">Test 2</div>
-            <div id="test3" class="col s12">Test 3</div>
-            <div id="test4" class="col s12">Test 4</div>
         </div>
-    </div>
+    <div id="test2" class="col s12"><p>Test 2</p></div>
+    <div id="test3" class="col s12"><p>Test 3</p></div>
+    <div id="test4" class="col s12"><p>Test 4</p></div>
+    <div id="test0" class="col s12"><p>Test 5</p></div>
 </div>
 
 
