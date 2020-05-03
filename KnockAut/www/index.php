748ae4d1ca8263734ee357e3348d7f2d4691fd35
Christian <c.fanciullo@me.com>
2020-05-03 11:08:27 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -67,14 +67,14 @@
         foreach($CategoryList2 as $Category2) { 
         
             $CatName2 = IPS_GetName($Category2);
-            $Var2 .= $CatName2; ?> </p>
+            $Var2 .= $CatName2; } ?> </p>
                         </div>
                         <div class="modal-footer">
                             <a href="#!" class="modal-close waves-effect waves-green btn-flat">Schliessen</a>
                         </div>
                     </div>          
                 </div>
-<!-- <?php } ?> -->
+
 
 <!-- <div id="output"> -->
 <script type="text/javascript" src="js/materialize.min.js"></script>
