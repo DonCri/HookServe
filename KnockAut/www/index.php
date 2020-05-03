9600d3539698165710996bdb5ba4e4e0e7ab43a3
Christian <c.fanciullo@me.com>
2020-05-03 10:55:37 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -18,6 +18,10 @@
    $('.modal').modal();
  });</script>
 
+ <script>$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})</script>
+
 </head>
 
 <body>
@@ -71,6 +75,16 @@
                     </div>          
                 </div>
  <?php } ?>
+ 
+
+ <div class="modal-body">
+  <h5>Popover in a modal</h5>
+  <p>This <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
+  <hr>
+  <h5>Tooltips in a modal</h5>
+  <p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
+</div>
+
 
 <!-- <div id="output"> -->
 <script type="text/javascript" src="js/materialize.min.js"></script>
