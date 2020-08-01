1f91ead2508d84bb53fae26381af385950ef2d62
Christian <c.fanciullo@me.com>
2020-08-01 20:44:08 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -37,7 +37,7 @@
       </ul>
     </div>
     <div id="test1" class="col s12"> <div id="MyHouse"><? include('include/kategorie.php'); ?> </div> </div>
-    <div id="test2" class="col s12">  </div>
+    <div id="test2" class="col s12"> <p id="output"> </p> </div>
     <div id="test3" class="col s12">Test 3</div>
     <div id="test4" class="col s12"> <a href="module_update.php"> Module Updaten </a> </div>
   </div>
