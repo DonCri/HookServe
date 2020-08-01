1cde1f68490800e2256656d5df56943b8b38f721
Christian <c.fanciullo@me.com>
2020-08-01 10:20:40 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -29,13 +29,13 @@
         <li class="tab col s3"><a href="#test1">My House</a></li>
         <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
         <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
-        <li class="tab col s3"><a href="#test4">Test 4</a></li>
+        <li class="tab col s3"><a href="#test4">Update</a></li>
       </ul>
     </div>
     <div id="test1" class="col s12">Test 1</div>
     <div id="test2" class="col s12">Test 2</div>
     <div id="test3" class="col s12">Test 3</div>
-    <div id="test4" class="col s12">Test 4</div>
+    <div id="test4" class="col s12"><? include("./module_update.php") ?></div>
   </div>
 
 <script type="text/javascript" src="js/materialize.min.js"></script>
