02e3ddc26dd239c7a175caf3b463059ce9cf95e4
Christian <c.fanciullo@me.com>
2020-08-01 10:31:01 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -27,13 +27,13 @@
     <div class="col s12">
       <ul class="tabs">
         <li class="tab col s3"><a href="#test1">My House</a></li>
-        <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
+        <li class="tab col s3"><a class="active" href="#test2">WebSocket</a></li>
         <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
         <li class="tab col s3"><a href="#test4">Update</a></li>
       </ul>
     </div>
     <div id="test1" class="col s12">Test 1</div>
-    <div id="test2" class="col s12">Test 2</div>
+    <div id="test2" class="col s12"> <? include("/ws/websocket.html") ?> </div>
     <div id="test3" class="col s12">Test 3</div>
     <div id="test4" class="col s12"> <a href="module_update.php"> Module Updaten </a> </div>
   </div>
