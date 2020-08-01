a9d1b970ab682271736f18ed1fe197708a830cea
Christian <c.fanciullo@me.com>
2020-08-01 10:59:00 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/index.php
+++ KnockAut/www/index.php
@@ -33,7 +33,7 @@
       </ul>
     </div>
     <div id="test1" class="col s12">Test 1</div>
-    <div id="test2" class="col s12"> <div id="output"> </div> </div>
+    <div id="test2" class="col s12"> <p id="output"> </p </div>
     <div id="test3" class="col s12">Test 3</div>
     <div id="test4" class="col s12"> <a href="module_update.php"> Module Updaten </a> </div>
   </div>
