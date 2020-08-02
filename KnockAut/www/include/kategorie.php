860a03132b9a1027d6c574f1d4b1f7834f8c7523
Christian <c.fanciullo@me.com>
2020-08-02 03:26:18 +0200

    Signed-off-by: Christian <c.fanciullo@me.com>

--- KnockAut/www/include/kategorie.php
+++ KnockAut/www/include/kategorie.php
@@ -13,7 +13,19 @@ $CategoryList = IPS_GetChildrenIDs(33603);
 foreach ($CategoryList as $Category) {
     $CategoryName = IPS_GetName($Category);
 
-    ?> <a class="waves-effect waves-light btn modal-trigger" href="#modal1"> <? echo($CategoryName); ?></a> <?
+    ?> <a class="waves-effect waves-light btn modal-trigger" href="#modal1"> <? echo($CategoryName); ?></a>
+    
+    <div id="modal1" class="modal">
+  <div class="modal-content">
+    <h4> <? echo($CategoryName); ?> </h4>
+    <p>A bunch of text</p>
+  </div>
+  <div class="modal-footer">
+    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+  </div>
+</div>
+    
+    <?
 } 
 ?>
 
@@ -22,15 +34,7 @@ foreach ($CategoryList as $Category) {
 
 
 <!-- Modal Structure -->
-<div id="modal1" class="modal">
-  <div class="modal-content">
-    <h4>Modal Header</h4>
-    <p>A bunch of text</p>
-  </div>
-  <div class="modal-footer">
-    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-  </div>
-</div>
+
 
 
 </body>
